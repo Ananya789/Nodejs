db714f69f0c16b5f50449bc1cc08531996cd655c
Ananya Chowdhury <ananyachowdhury@MacBook-Air.local>
2022-03-18 11:55:31 +0100

    deleting the files

--- test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-    const app = require("./index.js");
-
-    describe("GET /", () => {
-      it("respond with Hello World", (done) => {
-        request(app).get("/").expect("Hello World", done);
-      })
-    });
